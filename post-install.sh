fd4cad2f72a796818d73d2b06357e8b6fae9f9fc
imrisaac <imrisaac@gmail.com>
2025-02-18 12:59:09 -0500

    add metadata files and add accel deb install

--- post-install.sh
+++ post-install.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
-array=( https://extensions.gnome.org/extension/5389/screen-rotate/ )
+# install autorotate shell extension
+wget -P gnome-shell-extension-screen-autorotate https://github.com/apebl/gnome-shell-extension-screen-autorotate/archive/refs/heads/main.zip
+unzip gnome-shell-extension-screen-autorotate/main.zip -d gnome-shell-extension-screen-autorotate
+cd gnome-shell-extension-screen-autorotate
+cp -r screen-rotate@shyzus.github.io ~/.local/share/gnome-shell/extensions
+gnome-extensions enable screen-rotate@shyzus.github.io
 
-for i in "${array[@]}"
-do
-    EXTENSION_ID=$(curl -s $i | grep -oP 'data-uuid="\K[^"]+')
-    VERSION_TAG=$(curl -Lfs "https://extensions.gnome.org/extension-query/?search=$EXTENSION_ID" | jq '.extensions[0] | .shell_version_map | map(.pk) | max')
-    wget -O ${EXTENSION_ID}.zip "https://extensions.gnome.org/download-extension/${EXTENSION_ID}.shell-extension.zip?version_tag=$VERSION_TAG"
-    gnome-extensions install --force ${EXTENSION_ID}.zip
-    if ! gnome-extensions list | grep --quiet ${EXTENSION_ID}; then
-        busctl --user call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions org.gnome.Shell.Extensions InstallRemoteExtension s ${EXTENSION_ID}
-    fi
-    gnome-extensions enable ${EXTENSION_ID}
-    rm ${EXTENSION_ID}.zip
-done
+# install accel-sensor-bridge
+wget https://github.com/imrisaac/iio_accel_screen_rotation/blob/main/accel-sensor-bridge.deb
+sudo apt-get install -y ./accel-sensor-bridge.deb
+rm -rf accel-sensor-bridge.deb
+
+# Install cuda and gpu drivers
+#wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/sbsa/cuda-ubuntu2404.pin
+
+#udo mv cuda-ubuntu2404.pin /etc/apt/preferences.d/cuda-repository-pin-600
+
+#wget https://developer.download.nvidia.com/compute/cuda/12.8.0/local_installers/cuda-repo-ubuntu2404-12-8-local_12.8.0-570.86.10-1_arm64.deb
+
+#sudo dpkg -i cuda-repo-ubuntu2404-12-8-local_12.8.0-570.86.10-1_arm64.deb
+
+#sudo cp /var/cuda-repo-ubuntu2404-12-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
+
+#sudo apt-get update
+
+#sudo apt-get -y install cuda-toolkit-12-8
+
+#sudo apt-get install -y nvidia-open
